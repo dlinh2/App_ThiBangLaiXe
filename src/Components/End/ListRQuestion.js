e880e9b087f0e20ed449336f31a5f0f6ef87b123
dlinh2 <117155251+dlinh2@users.noreply.github.com>
2022-11-01 15:51:06 +0700

    Add files via upload

--- src/Components/End/ListRQuestion.js
+++ src/Components/End/ListRQuestion.js
@@ -5,18 +5,22 @@ import Question from "./ResultsQuestion";
 
 
 const ListQuestion = (props) => {
-    const {selectData} = props
+    
     return (
         <Wrapper>
             {
                 
                 Data.map(
                     (item, index) =>{  
+                        let select;
+                        for(let i = 1; i <props.length; i++){
+                            if(props[i].numQ === index + 1)
+                                return select = props[i].select
+                        }
                         return (
                             <Question
-                                select = {selectData[selectData.findIndex(obj => {
-                                    return obj.numQ === index + 1;
-                                    })].select}
+                                
+                                Select = {select}
                                 num = {index + 1}
                                 idQuestion = {index + 1}
                                 question = {item.question}
